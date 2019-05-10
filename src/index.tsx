bb7da550481c3e46e3040d90e94a01fff294a520
Mikolaj <mikolaj.a.marczak@gmail.com>
2019-05-10 23:06:38 +0200

    line

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,42 @@ import { render } from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+interface Props {
+  angle: number;
+  lenght: number;
+  origin: [number, number];
 }
 
-const rootElement = document.getElementById("root");
+const Line: React.FC<Props> = ({ angle, origin, lenght }) => {
+  const x: number = Math.sin(angle) * lenght;
+  const y: number = Math.cos(angle) * lenght;
+  const end: [number, number] = [origin[0] + x, origin[1] + y];
+  return (
+    <polyline
+      points={`${origin.join(",")} ${end.join(",")}`}
+      stroke="orange"
+      fill="transparent"
+      stroke-width="5"
+    />
+  );
+};
+
+const App = () => {
+  const test: number[] = [];
+  for (let i: number = 0; i <= 6.3; i += 0.1) {
+    test.push(i);
+  }
+
+  return (
+    <div className="App">
+      <svg viewBox="0 0 1000 1000">
+        {test.map(kont => (
+          <Line origin={[300, 300]} angle={kont} lenght={200} />
+        ))}
+      </svg>
+    </div>
+  );
+};
+
+const rootElement: HTMLElement | null = document.getElementById("root");
 render(<App />, rootElement);
