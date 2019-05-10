b9935490f397c1d8ac10e57965066eaa5110ef7b
Mikolaj <mikolaj.a.marczak@gmail.com>
2019-05-10 23:41:47 +0200

    0,0 in bottom left corner

--- src/index.tsx
+++ src/index.tsx
@@ -25,15 +25,21 @@ const Line: React.FC<Props> = ({ angle, origin, lenght }) => {
 
 const App = () => {
   const test: number[] = [];
-  for (let i: number = 0; i <= 6.3; i += 0.1) {
+  for (let i: number = 0; i <= 1.5; i += 0.1) {
     test.push(i);
   }
 
   return (
     <div className="App">
-      <svg viewBox="0 0 1000 1000">
+      <svg
+        viewBox="0 0 1000 1000"
+        style={{
+          backgroundColor: "white",
+          transform: "rotateX(180deg)"
+        }}
+      >
         {test.map(kont => (
-          <Line origin={[300, 300]} angle={kont} lenght={200} />
+          <Line key={kont} origin={[0, 0]} angle={kont} lenght={200} />
         ))}
       </svg>
     </div>
